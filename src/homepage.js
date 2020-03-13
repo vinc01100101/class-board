162993d275c5ecb50b8aa7caf3e2fe81e7a63a0e
vincE <vrytoledo@gmail.com>
2020-03-13 14:34:22 +0800

    Added feature notes

--- src/homepage.js
+++ src/homepage.js
@@ -1,14 +1,20 @@
 const React = require("react");
 module.exports = () => {
   return class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        showFeatures: false
+      };
+    }
     render() {
       const domSchoolsTxt = JSON.parse(
         document.getElementById("schools").textContent
       );
       return (
-        <div>
+        <div id="homepage">
           <h1>Welcome to ClassBoard!</h1>
-          <p style={{ color: "green" }}>(Under development)</p>
+
           <p>Every registered school will have their own customizable page.</p>
           <p>Navigate to your school using this url format: </p>
           <p>
@@ -40,7 +46,7 @@ module.exports = () => {
             1 Like = 5 hours of motivation boost to continue this project xD
           </h5>
           <div
-            class="fb-like"
+            className="fb-like"
             data-href="https://vince-class-board.glitch.me/"
             data-width="30"
             data-layout="box_count"
@@ -50,13 +56,75 @@ module.exports = () => {
           />
           <br />
           <br />
-          <a href="/api/drop-collection">
-            <span style={{ color: "red" }}>
-              Delete Collections From Database(Development Mode Only)
-            </span>
-          </a>
+          {/*---------DEVELOPMENT-----------*/}
+          <div
+            style={{
+              color: "green",
+              border: "green solid 3px",
+              width: "300px"
+            }}
+          >
+            This project is still under development Please click
+            <br />
+            <button
+              onClick={() => {
+                this.setState(c => {
+                  return { showFeatures: !c.showFeatures };
+                });
+              }}
+            >
+              here
+            </button>
+            <br />
+            to view the list of currently working features.
+            {this.state.showFeatures && (
+              <FeaturesAndUpdates
+                onC={() => {
+                  this.setState(c => {
+                    return { showFeatures: !c.showFeatures };
+                  });
+                }}
+              />
+            )}
+            <br />
+            <br />
+            <a href="/api/drop-collection">
+              <span style={{ color: "red" }}>
+                Delete Collections From Database(Development Mode Only)
+              </span>
+            </a>
+          </div>
+
+          {/*---------------------------------------*/}
         </div>
       );
     }
   };
 };
+
+function FeaturesAndUpdates(props) {
+  const listFeatures = [
+    `Register a school. (register a school together with the Owner/President's
+  account/email)`,
+    `Log in an Admin Account or use a ticket of the newly created admin
+  account.`,
+    `Control Panel button.`,
+    `Create Admin Account.`,
+    `Manage Admin Accounts (hover mouse on ID's to edit accounts).`
+  ];
+
+  const listUpdates = [];
+  return (
+    <div id="features-main-container">
+      <div id="secondary">
+        <h4>Currently Working Features:</h4>
+        <ol id="currently-working">
+          {listFeatures.map((x, i) => (
+            <li key={i}>{x}</li>
+          ))}
+        </ol>
+      </div>
+      <button onClick={props.onC}>CLOSE</button>
+    </div>
+  );
+}
